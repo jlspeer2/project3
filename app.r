d936432345dda429037012e40f768e3c9b025e8f
Jessica <jlspeer2@ncsu.edu>
2020-07-26 23:41:52 -0400

    RStudio

--- app.r
+++ app.r
@@ -2,42 +2,94 @@ library(shiny)
 library(shinydashboard)
 
 ui <- dashboardPage(
-  dashboardHeader(title = "Basic dashboard"),
+  dashboardHeader(title = "Project3 Shiny App"),
   dashboardSidebar(
     sidebarMenu(
-      menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
-      menuItem("Widgets", tabName = "widgets", icon = icon("th"))
+      menuItem("Models", tabName = "models"),
+      menuItem("intro", tabName = "intro")
     )
   ),
   dashboardBody(
     tabItems(
       # First tab content
-      tabItem(tabName = "dashboard",
+      tabItem(tabName = "models",
               fluidRow(
-                box(plotOutput("plot1", height = 250)),
                 
                 box(
-                  title = "Controls",
-                  sliderInput("slider", "Number of observations:", 1, 100, 50)
+                  selectizeInput("mod1rep", "Response Variable", selected = "sex", 
+                                   choices = c("sex","school", "activities", "romantic")
+                  )
+                ),
+                                     
+                box(
+                  width=11,verbatimTextOutput("mod1sum")
+                ),
+                
+                box(
+                  width=7,
+                  checkboxInput("mod1pred", h4("Make predictions using default model (response=sex)")),
+                  conditionalPanel(condition = "input.mod1pred == true",
+                    selectizeInput("schoolp", "School", selected = "GP", 
+                                   choices = c("GP", "MS")
+                    ),
+                    selectizeInput("activp", "Activities", selected = "yes", 
+                                   choices = c("yes", "no")
+                    ),
+                    selectizeInput("romp", "Romantic partnership", selected = "yes", 
+                                   choices = c("yes", "no")
+                    ),
+                    selectizeInput("romp", "Romantic partnership", selected = "yes", 
+                                   choices = c("yes", "no")
+                    ),
+                    sliderInput("studyp", "Study Time", min = 1, max = 4, value=2, step = 1),
+                    sliderInput("famp", "Quality of Family Relationship", min = 1, max = 5, 
+                                value=2, step = 1),
+                    sliderInput("freep", "Free Time", min = 1, max = 5, value=2, step = 1),
+                    sliderInput("freep", "Out with Friends (Frequency)", min = 1, max = 5, 
+                                value=2, step = 1),
+                    sliderInput("walcp", "Weekend Alcohol Consumption", min = 1, max = 5, 
+                                value=2, step = 1)
+                  )
                 )
               )
       ),
       
       # Second tab content
-      tabItem(tabName = "widgets",
+      tabItem(tabName = "intro",
               h2("Widgets tab content")
       )
+      )
     )
-  )
 )
 
-server <- function(input, output) {
-  set.seed(122)
-  histdata <- rnorm(500)
   
-  output$plot1 <- renderPlot({
-    data <- histdata[seq_len(input$slider)]
-    hist(data)
+
+
+
+
+server <- function(input, output) {
+  
+  getData <- reactive({
+    data=read.table("data/student-por.csv",sep=";",header=TRUE)
+    data <- data %>% select(sex, school, studytime, activities, romantic, famrel,
+                            freetime, goout, Walc)
+  })
+  
+  output$mod1sum <- renderPrint({
+    data <- getData()
+    if(input$mod1rep=="sex"){
+      mod1<-glm(sex~., data=data, family="binomial")
+    }
+    if(input$mod1rep=="school"){
+      mod1<-glm(school~., data=data, family="binomial")
+    }
+    if(input$mod1rep=="activities"){
+      mod1<-glm(activities~., data=data, family="binomial")
+    }
+    if(input$mod1rep=="romantic"){
+      mod1<-glm(romantic~., data=data, family="binomial")
+    }
+    summary(mod1)
   })
 }
 
--- data/dataprocessing.R
+++ data/dataprocessing.R
@@ -1,9 +1,37 @@
 library(dplyr)
+library(tidyverse)
+library(caret)
 
-d1=read.table("data/student-mat.csv",sep=";",header=TRUE)
-d2=read.table("data/student-por.csv",sep=";",header=TRUE)
 
-d3=merge(d1,d2,by=c("school","sex","age","address","famsize","Pstatus",
-                    "Medu","Fedu","Mjob","Fjob","reason","nursery","internet"))
-print(nrow(d3)) # 382 students
+data=read.table("data/student-por.csv",sep=";",header=TRUE)
+data <- data %>% select(-c(G1, G2))
+
+mod1<-glm(sex~school + studytime + activities + romantic +
+            famrel + freetime + goout + Walc, data=data, family="binomial")
+summary(mod1)
+
+set.seed(1)
+train <- sample(1:nrow(data), size = nrow(data)*0.8)
+test <- dplyr::setdiff(1:nrow(data), train)
+dataTrain <- data[train, ]
+dataTest <- data[test, ]
+
+trctrl <- trainControl(method = "repeatedcv", number = 3, repeats = 1)
+
+knn_fit <- train(sex ~ school + studytime + activities + romantic +
+                   famrel + freetime + goout + Walc, data = dataTrain, method = "knn", 
+                   trControl=trctrl, preProcess = c("center", "scale"))
+plot(knn_fit)
+#make predictions on test data
+test_pred <- predict(knn_fit, newdata = dataTest)
+res <- confusionMatrix(test_pred, dataTest$sex)
+res
+#misclassification rate
+1-sum(diag(res$table))/sum(res$table)
+
+
+
+
+
+
 
